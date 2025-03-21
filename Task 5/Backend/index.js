8c60bb29e14122d4e138f6679bada0edc24467f1
Suprakash Gorai <92419286+suprakash-144@users.noreply.github.com>
2025-03-22 00:22:03 +0530

    Backend

---
+++ Task 5/Backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dbConnect = require("./Config/dbConnect");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const Authrouter = require("./Routes/Authroute");
+const Todorouter = require("./Routes/Todoroute");
+const { notFound, errorHandler } = require("./Middlewares/errorHandler");
+const app = express();
+const cors = require("cors");
+
+const cookieParser = require("cookie-parser");
+const swagger = require("./swagger");
+dbConnect();
+
+// ?middleware
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+//  sawgger setup
+swagger(app);
+
+// Routes
+app.use("/todo", Todorouter);
+app.use("/", Authrouter);
+
+//error handling
+app.use(notFound);
+app.use(errorHandler);
+// server config
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running  at  http://localhost:${process.env.PORT}/`);
+  console.log(
+    `Swagger is running  at http://localhost:${process.env.PORT}/docs`
+  );
+});
